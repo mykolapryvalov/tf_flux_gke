d20e80c3546617a0b549b2005c33c1bb97afc688
mykolapryvalov <nikpryvalov@gmail.com>
2024-05-16 10:15:16 +0000

    update

--- main.tf
+++ main.tf
@@ -1,13 +1,38 @@
-module "gke_cluster" {
-  source         = "git::https://github.com/mykolapryvalov/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
-}
-
 terraform {
   backend "gcs" {
-    bucket = "my-secret"
-    prefix = "terraform/state"
+    bucket  = "vit-secret"
+    prefix  = "terraform/state"
   }
 }
+
+module "github_repository" {
+  source                   = "github.com/mykolapryvalov/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux"
+}
+
+module "tls_private_key" {
+  source    = "github.com/mykolapryvalov/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
+}
+
+# Source: "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+module "kind_cluster" {
+  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+}
+
+# # Source: "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+module "flux_bootstrap" {
+  source            = "./modules/flux_bootstrap/"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_host       = module.kind_cluster.endpoint
+  config_client_key = module.kind_cluster.client_key
+  config_ca         = module.kind_cluster.ca
+  config_crt        = module.kind_cluster.crt
+  github_token      = var.GITHUB_TOKEN
+}
+
--- variables.tf
+++ variables.tf
@@ -1,12 +1,33 @@
 variable "GOOGLE_PROJECT" {
   type        = string
-  default     = ""
   description = "GCP project to use"
 }
 
 variable "GOOGLE_REGION" {
-  description = "The Google Cloud region to deploy resources"
   type        = string
-  # You can optionally set a default value here
-  # default     = "us-central1"
+  default     = "us-central1-c"
+  description = "GCP region to use"
 }
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
+}
+
