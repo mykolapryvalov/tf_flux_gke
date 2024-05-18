c58f5451ad8635120b12d4a9cfcb766c203c1c83
MP <53264992+mykolapryvalov@users.noreply.github.com>
2024-05-18 23:12:04 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,20 +19,18 @@ module "tls_private_key" {
   algorithm = "RSA"
 }
 
-# Source: "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
-module "kind_cluster" {
-  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+module "gke_cluster" {
+  source         = "github.com/mykolapryvalov/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
 }
 
-# # Source: "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
 module "flux_bootstrap" {
   source            = "./modules/flux_bootstrap/"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
-  config_host       = module.kind_cluster.endpoint
-  config_client_key = module.kind_cluster.client_key
-  config_ca         = module.kind_cluster.ca
-  config_crt        = module.kind_cluster.crt
+  config_path       = module.gke_cluster.kubeconfig
   github_token      = var.GITHUB_TOKEN
 }
 
